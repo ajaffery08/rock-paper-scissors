09a632be7f9bd236172a3303b420d1911eedebfd
Ahmed Jaffery <ajaffery08@gmail.com>
2021-05-25 20:52:18 -0400

    game part 1
    incorporated the basic game functionality

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # rock-paper-scissors
 for python class
+
+step 1
+so far there is just a print function for rock paper scissors shoot
+
+step 2
--- game.py
+++ game.py
@@ -1,7 +1,36 @@
 
 # game.py
 
+import random
+
+
 print("Rock, Paper, Scissors, Shoot!")
+#print(10)
 
-print(10)
+user_choice = input("Please choose one of 'rock', 'paper', 'scissors': ")
 
+#print(user_choice)
+print("USER CHOICE: ", user_choice)
+
+
+
+#validate the input such that only if it is one of the expected values
+#...will we continue with the rest of the program
+#...otherwise we'll stop the program before it tries to do anything else
+#...and we'll ask the user to run the program again
+
+#and
+#or
+
+
+if (user_choice == "rock") or (user_choice == "paper") or (user_choice == "scissors"):
+    print("VALID. KEEP GOING")
+else:
+    print("Oops, invalid input. Please try again.")
+    exit()
+
+valid_options = ["rock","paper","scissors"]
+computer_choice = random.choice(valid_options)
+print("COMPUTER CHOICE: ", computer_choice)
+
+print ("THIS IS THE END OF OUR GAME. PLEASE PLAY AGAIN.")
