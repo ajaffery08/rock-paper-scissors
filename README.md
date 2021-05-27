d2cf97b1cb02636a434b46d25c93f1fffc43d527
Ahmed Jaffery <ajaffery08@gmail.com>
2021-05-27 19:02:21 -0400

    final game version

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ for python class
 step 1
 so far there is just a print function for rock paper scissors shoot
 
-step 2
+step 2 now the game is good to go. user should input their name in .env file and then ply the game. good luck! 
--- game.py
+++ game.py
@@ -2,9 +2,16 @@
 # game.py
 
 import random
+import dotenv
+import os
+
+dotenv.load_dotenv()
+
+PLAYER_NAME = os.getenv("PLAYER_NAME")
+print(PLAYER_NAME)
 
 
-print("Rock, Paper, Scissors, Shoot!")
+print("Rock, Paper, Scissors, Shoot! Welcome", PLAYER_NAME, "!")
 #print(10)
 
 user_choice = input("Please choose one of 'rock', 'paper', 'scissors': ")
@@ -32,5 +39,26 @@ else:
 valid_options = ["rock","paper","scissors"]
 computer_choice = random.choice(valid_options)
 print("COMPUTER CHOICE: ", computer_choice)
+
+#determine who won! 
+
+if user_choice == computer_choice:
+    print("It's a tie, try again!")
+elif (user_choice == "scissors") and (computer_choice == "paper"):
+    print("Scissors cuts paper, you win!")
+elif (user_choice == "rock") and (computer_choice == "scissors"):
+    print("rock smashes scissors, you win!")
+elif (user_choice == "paper") and (computer_choice == "rock"):
+    print("Paper covers rock, you win!")
+elif (user_choice == "scissors") and (computer_choice == "rock"):
+    print("rock smashes scissors, you lose!")
+elif (user_choice == "rock") and (computer_choice == "paper"):
+    print("paper covers rock, you lose!")
+elif (user_choice == "paper") and (computer_choice == "scissors"):
+    print("Scissors cuts paper, you lose!")
+
+#configure player name via envrionment variables
+
+
 
 print ("THIS IS THE END OF OUR GAME. PLEASE PLAY AGAIN.")
